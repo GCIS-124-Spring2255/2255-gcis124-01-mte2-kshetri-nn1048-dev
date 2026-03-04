a84c92b159380b99dc9f32bb737408d51f487ac5
Nikhil <nn1048@rit.edu>
2026-03-04 10:58:50 -0500

    Completed Question 1 for Exam 2

--- src/main/java/mte2/shapes/Circle.java
+++ src/main/java/mte2/shapes/Circle.java
@@ -4,18 +4,24 @@
 
 package mte2.shapes;
 
-public class Circle {
-// public class Circle implements Shape {
+//public class Circle {
+public class Circle implements Shape {
     
-    // private final double radius;
-    // public Circle(double radius) {    this.radius = radius;    }
+    private final double radius;
+    public Circle(double radius) {    this.radius = radius;    }
 
-    // @Override
-    // @Override
+    @Override
+    public double perimeter() {
+        return ((2*3.14)*(radius));
+    }
+    @Override
+    public double area() {
+        return ((3.14)*(radius*radius));
+    }
 
     public static void main(String[] args) {
-        
-        // ...
-
+        Shape circle = new Circle(6);
+        System.out.println(circle.perimeter());
+        System.out.println(circle.area());
     }
 }
--- src/main/java/mte2/shapes/Triangle.java
+++ src/main/java/mte2/shapes/Triangle.java
@@ -2,24 +2,31 @@
 // Package name: shapes
 // File name: Triangle.java
 
+
 package mte2.shapes;
 
-public class Triangle {
-// public class Triangle implements Shape {
-    // private double sideA;
-    // private double sideB;
-    // private double sideC;
+//public class Triangle {
+public class Triangle implements Shape {
+    private double sideA;
+    private double sideB;
+    private double sideC;
 
-    // public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
+    public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
 
-    // @Override
+    @Override
+    public double perimeter() {
+        return (sideA+sideB+sideC);
+    }
 
-    // @Override
-    // public double area() {    double p = perimeter()  / 2;    }
+    @Override
+    public double area() {    
+        double s = perimeter() / 2;
+        return Math.sqrt((s*(s-sideA)*(s-sideB)*(s-sideC)));
+    }
 
     public static void main(String[] args) {
-        
-        // ...
-
+        Shape triangle = new Triangle(4,4,4);
+        System.out.println(triangle.perimeter());
+        System.out.println(triangle.area());
     }
 }
